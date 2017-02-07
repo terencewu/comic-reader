27289450fa81a48c29a7473826cd33764f412b51
Terence Wu <twu429@gmail.com>
2017-02-06 20:37:45 -0800

    uncompress.js now added

--- main.js
+++ main.js
@@ -2,10 +2,13 @@
 
 var img = document.getElementById("main-image");
 var imgcontainer = document.getElementById("main-image-container");
-var filecontent = null;
-var filelist = null;
-var index = 0;
-var mode = 0;
+var file_name = document.getElementById("file_name");
+var page_count = document.getElementById("page_count");
+var page_number = document.getElementById("page_number");
+// page_number.oninput = choosePage; 
+// page_number.onpropertychange = page_number.oninput;
+page_number.onkeypress = choosePage;
+
 var imageWidth = 0;
 var imageHeight = 0;
 var screenwidth = document.documentElement.clientWidth;
@@ -15,6 +18,33 @@ var nextpage = false;
 var scrollcurrent = 0;
 var scrollnext = 0;
 var autoscrolling = false;
+var archive = null;
+var mode = 0;
+
+
+
+function choosePage(evt) {
+  console.log("input change detected");
+  console.log(evt);
+  console.log(evt.target.value);
+
+  if (!evt) evt = window.event;
+  var keyCode = evt.keyCode || evt.which;
+  if (keyCode == '13'){ // Enter pressed
+    console.log("pressed enter");
+    console.log(page_number);
+    console.log(page_number.value);
+    //check if page_number.value is an integer
+    if(parseInt(page_number.value) === NaN) {
+      return false;
+    }
+    var pageindex = parseInt(page_number.value) - 1;
+    console.log("goto!");
+    //update global page index
+    archive.gotopage(pageindex);
+  }  
+
+}
 
 function scrollTo(element, destination, duration = 500, callback) {
 
@@ -38,8 +68,6 @@ function scrollTo(element, destination, duration = 500, callback) {
   }
   scroll();
 }
-
-
 
 
 function addClass(el, cl) {
@@ -84,11 +112,6 @@ function step() {
   case 0: //scrolling down
     addClass(imgcontainer, 'fit-width-step');
 
-    console.log(scrollcurrent);
-    console.log(scrollnext);
-    console.log(imgcontainer.scrollHeight - imgcontainer.clientHeight);
-    console.log(((imgcontainer.scrollHeight - imgcontainer.clientHeight) <= 0));
-    console.log("----------------------------");
     scrollnext = scrollcurrent + (screenheight/2);
 
     /*
@@ -121,7 +144,7 @@ function step() {
     break;
   case 1: //scrolling left/right
     addClass(imgcontainer, 'fit-height-step');
-    imgcontainer.scrollLeft = step_index * (screenwidth/2);
+    // imgcontainer.scrollLeft = step_index * (screenwidth/2);
     break;
   default:
     break;
@@ -130,70 +153,25 @@ function step() {
 }
 
 function prev() {
-  if(index < 0 || index >= filelist.length) {
-    return ;
-  }
-  index -= 1;
-  gotopage(index);
+  archive.prev();
 }
 
 function next() {
-  if(index < 0 || index >= filelist.length) {
-    return ;
-  }
-  index += 1;
-  gotopage(index);
+  archive.next();
 }
 
-function gotopage(index) {
-  var filename = null;
-  filename = filelist[index];
-
-  //gets file extension
-  var ext = filename.substr((~-filename.lastIndexOf(".") >>> 0) + 2);
-
-  if(ext !== "jpg" && ext !== "jpeg" && ext !== "png") {
-    console.log("extension : " + ext + " is not supported.");
-    return next();
-  }
-  console.log(index + "/" + filelist.length);
-  //get this file and return it as base64 encoded data
-  filecontent.file(filename).async("base64").then(function(imgdata) {
-
-    img.onload = function() {
-      imageWidth = img.clientWidth;
-      imageHeight = img.clientHeight;
-    }
-
-    img.src= 'data:image/*;base64, ' + imgdata;
-    imgcontainer.scrollTop = 0;
-  });
-
-}
-
-function unzip(content) {
-  index = 0;
-  var zip = new JSZip();
-  zip.loadAsync(content)
-    .then(function(rzip) {
-      // you now have every files contained in the loaded zip
-      filecontent = rzip;
-      filelist = Object.keys(rzip.files).sort();
-      console.log(filecontent);
-      next();
-    });
+function loadinfo(fname, pgnum, pgcount) {
+  file_name.innerHTML = fname;
+  page_number.value = pgnum + 1; //displays + 1 because 0 is first index.
+  page_count.innerHTML = pgcount;
 }
 
 function handleFileSelect(evt) {
   var files = evt.target.files; // FileList object
 
-  resetOriginal();
-  clearClasses();
-
-  //zip files
-  unzip(files[0]);
-
-  //tar files
+  //manually loads the type of archive formats for reading (required for uncompress.js)
+  loadArchiveFormats(['rar', 'zip', 'tar']);
+  archive = new _archive(files[0]);
 }
 
 imgcontainer.onscroll = function(evt) {
@@ -207,6 +185,105 @@ imgcontainer.onscroll = function(evt) {
 
 document.getElementById('upload').addEventListener('change', handleFileSelect, false);
 
-// if (document.readyState === "complete" || (document.readyState !== "loading" && !document.documentElement.doScroll)) {
-//     main();
-// }
+var stateCheck = setInterval(function() {
+  if (document.readyState === "complete" || (document.readyState !== "loading" && !document.documentElement.doScroll)) {
+    clearInterval(stateCheck);
+    console.log("ready");
+    console.log(imgcontainer);
+
+  }
+}, 100);
+
+
+//read left to right
+//read right to left
+
+//show two images at once
+
+
+function Unarchiver(archive) {
+  var self = this;
+  self.archive = archive;
+  self.index = 0;
+  self.archivecontent = [];
+  self.gotopage = function(index) { 
+    if(0 <= index && index < self.archivecontent.length) {
+      self.index = index;
+      self._gotopage(index);
+    }
+  }
+  self._gotopage = function(index) { return null; }
+  self.archivename = function() { return null; }
+  self.pagename = function() { return null; }
+  self.filecount = function() { return null; }
+  self.isImage = function(filename="") {
+    if(!filename) {
+      return false;
+    }
+    //gets file extension
+    var ext = filename.substr((~-filename.lastIndexOf(".") >>> 0) + 2);
+    
+    if(ext !== "jpg" && ext !== "jpeg" && ext !== "png" && ext !== "webp" && ext !== "bmp" && ext !== "gif") {
+      console.log("extension : " + ext + " is not supported.");
+      return false;
+    }
+    return true;
+  }
+  self.next = function() {
+    if(0 <= self.index && self.index < self.archivecontent.length) {
+      self.index += 1;
+      self.gotopage(self.index);    
+    }
+  }
+
+  self.prev = function() {
+    if(0 < self.index && self.index < self.archivecontent.length) {
+      self.index -= 1;
+      self.gotopage(self.index);    
+    }
+  }
+}
+
+function _archive(archive) {
+  Unarchiver.call(this, archive);
+  var self = this;
+
+  self._gotopage = function(index) {
+    var entry = self.archivecontent[index];
+    var pages = self.archivecontent.length;
+
+    if(!self.isImage(entry.name)) {
+      return self.next();
+    }
+    var mimetype = "image/*";
+
+    entry.readData(function(data, err) {
+		  var blob = new Blob([data], {type: mimetype});
+		  var imgurl = URL.createObjectURL(blob);
+      img.onload = function() {
+        imageWidth = img.clientWidth;
+        imageHeight = img.clientHeight;
+        URL.revokeObjectURL(imgurl); //free object
+      }
+
+      loadinfo(entry.name, index, pages)
+      img.src = imgurl;
+
+      imgcontainer.scrollTop = 0;
+    });    
+  }
+
+  //load this! or we either put this as a constructor/prototype?
+
+  archiveOpenFile(archive, function(archivecontent, err) {
+    if(err) {
+      console.log(err);
+    }
+    if(archivecontent) {
+      self.archive = null;
+      self.archivecontent = archivecontent.entries;
+      self.gotopage(self.index);
+    }
+  });
+  
+}
