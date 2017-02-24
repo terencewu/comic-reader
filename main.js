e184e14b4723661e692c2065c6b7eaa014aa0ceb
Terence Wu <twu429@gmail.com>
2017-02-23 18:18:24 -0800

    refactored the step function for improved readability

--- main.js
+++ main.js
@@ -22,6 +22,8 @@ var archive = null;
 var mode = 0;
 var left2right = true;
 
+var classlist = ["fit-height","fit-width","fit-width-step","fit-height-step"];
+var imgext = ["jpg", "jpeg", "png", "webp", "bmp", "gif"];
 
 function choosePage(evt) {
   console.log("input change detected");
@@ -50,16 +52,15 @@ function toggleDirection() {
 
 function scrollTo(element, destination, scrolldirection = "down", duration = 500, callback) {
   if(scrolldirection === "down") {
+    console.log("top2bottom");
     scrolldirection = "scrollTop";
   }
   else if(scrolldirection === "left") {
     console.log("left2right");
     scrolldirection = "scrollLeft";
-    //scrolldirection = "scrollRight";
   }
   else if(scrolldirection === "right") {
     console.log("right2left");
-    // scrolldirection = "scrollLeft";
     scrolldirection = "scrollRight";
   }
 
@@ -102,11 +103,15 @@ function removeClass(el, cl) {
   el.className = el.className.replace(new RegExp('(\\s|^)' + cl + '(\\s|$)', 'g'), ' ').replace(/(^\s+|\s+$)/g,'');
 }
 
+
 function clearClasses() {
-  removeClass(imgcontainer, 'fit-height');
-  removeClass(imgcontainer, 'fit-width');
-  removeClass(imgcontainer, 'fit-width-step');
-  removeClass(imgcontainer, 'fit-height-step');
+  for(var i = 0; i < classlist.length; i++) {
+    removeClass(imgcontainer, classlist[i])
+  }
+  // removeClass(imgcontainer, 'fit-height');
+  // removeClass(imgcontainer, 'fit-width');
+  // removeClass(imgcontainer, 'fit-width-step');
+  // removeClass(imgcontainer, 'fit-height-step');
 }
 
 function resetOriginal() {
@@ -139,6 +144,10 @@ function fitheightstep() {
   addClass(imgcontainer, 'fit-height-step');
 }
 
+function updatesteptext(newtext) {
+  step_elem.innerHTML = newtext;
+}
+
 function step() {
   if(autoscrolling) {
     //prevents user from pressing too quickly for step() to resolve
@@ -146,98 +155,87 @@ function step() {
   }
 
   clearClasses();
-
-  //set width based on natural image width
-  switch (mode) {
-  case 0: //scrolling down
-    addClass(imgcontainer, 'fit-width-step');
-
-    scrollnext = imgcontainer.scrollTop + (screenheight/2);
-    /*
-      1. No scrolling needed
-      2. nextpage triggered.
-     */
-    if( ((imgcontainer.scrollHeight - imgcontainer.clientHeight) <= 0) ||
-        (nextpage && (imgcontainer.scrollHeight - imgcontainer.clientHeight) <= scrollnext )
-      ) {
-      console.log("going to next page");
-      scrollnext = 0;
-      nextpage = false;
-      step_elem.innerHTML = "Scroll<br>Down";
-      next();
-    }
-    else {
-      if((imgcontainer.scrollHeight - imgcontainer.clientHeight) <= scrollnext) {
-        console.log("next page");
-
-        //update button text
-        step_elem.innerHTML = "Next<br>Page";
-        nextpage = true;
-      }
-
-      console.log("scroll down");
-      autoscrolling = true;
-      scrollTo(imgcontainer,  scrollnext, "down", 500, function() {
-        autoscrolling = false;
-     });
-    }
-    break;
-
-  case 1: //scrolling left/right
-    addClass(imgcontainer, 'fit-height-step');
-
-    //set text
-    step_elem.innerHTML ="Scroll<br>Right";
-
-    scrollnext = imgcontainer.scrollLeft + (screenwidth/2);
-    /*
-      1. No scrolling needed
-      2. nextpage triggered.
-     */
-    console.log(((imgcontainer.scrollWidth - imgcontainer.clientWidth) <= 0) ||
-        (nextpage && (imgcontainer.scrollWidth - imgcontainer.clientWidth) <= scrollnext ));
-
-    if( ((imgcontainer.scrollWidth - imgcontainer.clientWidth) <= 0) ||
-        (nextpage && (imgcontainer.scrollWidth - imgcontainer.clientWidth) <= scrollnext )
-      ) {
-      console.log("going to next page");
-      scrollnext = 0;
-      nextpage = false;
-      step_elem.innerHTML ="Scroll<br>Right";
-      next();
-    }
-    else {
-      if((imgcontainer.scrollWidth - imgcontainer.clientWidth) <= scrollnext) {
-        //update button text
-        step_elem.innerHTML = "Next<br>Page";
-
-        console.log("next page");
-        nextpage = true;
-      }
-
-      console.log("scroll left/right");
-      autoscrolling = true;
-
-      /*
-        reading direction
-        "left" = left 2 right
-        "right" = right 2 left
-      */                
-      var direction = "left";
-      if(!left2right) {
-        direction = "right";
-      }
-
-      scrollTo(imgcontainer, scrollnext, direction, 500, function() {
-        autoscrolling = false;
-     });
-    }
-    break;
-
-  default:
-    break;
-  }
   
+  var stepclass = '';
+  var steptext = 'Scroll';
+  var scrollposition = 0;
+  var scrolllength = 0;
+  var clientlength = 0;
+
+  var direction = '';
+  var screenhalf = 0;
+
+  if(mode == 0) {
+    stepclass = 'fit-width-step';
+    //class must be set properly in order to values to be correct
+    addClass(imgcontainer, stepclass);
+    
+    steptext = 'Scroll<br>Down';
+    scrollposition = imgcontainer.scrollTop;
+    scrolllength = imgcontainer.scrollHeight;
+    clientlength = imgcontainer.clientHeight;
+    screenhalf = screenheight/2;
+    direction = "down";
+  }
+  else if(mode == 1) {
+    stepclass = 'fit-height-step';
+    addClass(imgcontainer, stepclass);
+
+    scrolllength = imgcontainer.scrollWidth;
+    clientlength = imgcontainer.clientWidth;
+    screenhalf = screenwidth/2; 
+    /*
+      reading direction
+      "left" = left 2 right
+      "right" = right 2 left
+    */     
+    // direction = left2right ? "left" : "right";
+
+    if(left2right) {
+      direction = "left";
+      steptext = 'Scroll<br>Left';
+      scrollposition = imgcontainer.scrollLeft;      
+    }
+    else {
+      direction = "right";
+      steptext = 'Scroll<br>Right';
+      scrollposition = imgcontainer.scrollRight;        
+    }
+  }
+
+  updatesteptext(steptext);
+
+  scrollnext = scrollposition + screenhalf;
+  /*
+    1. No scrolling needed
+    2. nextpage triggered.
+  */
+
+  console.log(screenhalf);
+  console.log(scrollposition);
+  console.log(scrolllength);
+  console.log(scrollnext);
+
+  if(nextpage && ((scrolllength - clientlength) <= scrollnext)) {
+    console.log("going to next page");
+    scrollnext = 0;
+    nextpage = false;
+    updatesteptext(steptext);
+    next();
+  }
+  else {
+    if((scrolllength - clientlength) <= scrollnext) {
+      updatesteptext("Next<br>Page");
+      console.log("next page");
+      nextpage = true;
+    }
+    autoscrolling = true;
+    console.log(direction);
+    scrollTo(imgcontainer, scrollnext, direction, 500, function() {
+      autoscrolling = false;
+    });
+  }
+
 }
 
 function prev() {
@@ -280,8 +278,6 @@ var stateCheck = setInterval(function() {
   if (document.readyState === "complete" || (document.readyState !== "loading" && !document.documentElement.doScroll)) {
     clearInterval(stateCheck);
     console.log("ready");
-    console.log(imgcontainer);
-
   }
 }, 100);
 
@@ -315,16 +311,24 @@ function Unarchiver(archive) {
     //gets file extension
     var ext = filename.substr((~-filename.lastIndexOf(".") >>> 0) + 2);
     
-    if(ext !== "jpg" && ext !== "jpeg" && ext !== "png" && ext !== "webp" && ext !== "bmp" && ext !== "gif") {
-      console.log("extension : " + ext + " is not supported.");
-      return false;
+    // if(ext !== "jpg" && ext !== "jpeg" && ext !== "png" && ext !== "webp" && ext !== "bmp" && ext !== "gif") {
+    //   console.log("extension : " + ext + " is not supported.");
+    //   return false;
+    // }
+
+    //checking this way is inefficient. (but is currently neglible) Will be better off saving this data in an array of objects after a single pass.
+    for(var i = 0; i < imgext.length; i++) {
+      if(ext === imgext[i]) {
+        console.log("extension : " + ext + " is not supported.");
+        return true;
+      }
     }
-    return true;
+
+    return false;
   }
   self.next = function() {
     if(0 <= self.index && self.index < self.archivecontent.length - 1) {
       self.index += 1;
-      console.log(self.index);
       self.gotopage(self.index);    
     }
   }
@@ -332,7 +336,6 @@ function Unarchiver(archive) {
   self.prev = function() {
     if(0 < self.index && self.index < self.archivecontent.length) {
       self.index -= 1;
-      console.log(self.index);
       self.gotopage(self.index);    
     }
   }
@@ -361,16 +364,19 @@ function _archive(archive) {
 
         if(img.naturalWidth > img.naturalHeight) {
           if(left2right) {
-            step_elem.innerHTML ="Scroll<br>Right";
+            // step_elem.innerHTML ="Scroll<br>Right";
+            updatesteptext("Scroll<br>Left");
           }
           else {
-            step_elem.innerHTML ="Scroll<br>Left";
+            // step_elem.innerHTML ="Scroll<br>Left";
+            updatesteptext("Scroll<br>Right");
           }
           fitheightstep();
           mode = 1;
         }
         else {  //squares or height > width
-          step_elem.innerHTML ="Scroll<br>Down";
+          // step_elem.innerHTML ="Scroll<br>Down";
+          updatesteptext("Scroll<br>Down");
           fitwidthstep();
           mode = 0;
         }
